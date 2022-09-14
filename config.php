1028abcc9c2d9df6aece384f3762c532f5dcbd04
paulovictor1997 <paulovictornunes97@gmail.com>
2022-09-14 12:06:32 -0300

    Update

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    define('HOST', 'ftp.clickevolue.dev.br');
-    define('USER', 'clickevolue7');
-    define('PASS', 'Cl1ckEvolue#2022');
-    define('BASE', 'clickevolue7');
-
-    $conn = new mysqli(HOST,USER,PASS,BASE);
-    
-?>
