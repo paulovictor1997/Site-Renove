20a373ca50b470a56fbe414245d1e339d6d0dc25
paulovictor1997 <paulovictornunes97@gmail.com>
2022-09-14 00:54:28 -0300

    Update

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,21 +1,20 @@
-// MENU 
-const hamburguer = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+function openMenu(){
+    const openButton = document.querySelector('.open-button');
+    const closeButton = document.querySelector('.close-button');
+    const menu = document.querySelector('.menu');
 
-hamburguer.addEventListener("click",()=>{
-    hamburguer.classList.toggle('active');
-    navMenu.classList.toggle('active');
-});
+    openButton.classList.add('hidden');
+    closeButton.classList.remove('hidden');
+    menu.classList.add('show');
+    
+}
 
-// CONFIGURANDO O SLIDE 
-new Glider(document.querySelector('.glider'), {
-    slidesToShow: 1,
-    draggable: true,
-    arrows: {
-      prev: '.glider-prev',
-      next: '.glider-next'
-    }
-});
- 
+function closeMenu(){
+    const openButton = document.querySelector('.open-button');
+    const closeButton = document.querySelector('.close-button');
+    const menu = document.querySelector('.menu');
 
-  
+    openButton.classList.remove('hidden');
+    closeButton.classList.add('hidden');
+    menu.classList.remove('show');
+}
