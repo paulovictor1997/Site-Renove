d824ed729c6b85d6e69e66ae3cdc03f47ba5b1e8
paulovictor1997 <paulovictornunes97@gmail.com>
2022-09-16 22:54:40 -0300

    Update menu

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,4 @@
+//Abrir e fechar menu
 function openMenu(){
     const openButton = document.querySelector('.open-button');
     const closeButton = document.querySelector('.close-button');
@@ -17,4 +18,16 @@ function closeMenu(){
     openButton.classList.remove('hidden');
     closeButton.classList.add('hidden');
     menu.classList.remove('show');
+}
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    const scroll = document.querySelector('#scrollMenu');
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scroll.style.backgroundColor = '#00416B';
+  } else {
+    scroll.style.backgroundColor = 'transparent';
+  }
 }
